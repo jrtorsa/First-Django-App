538f41b79aeac8d23a18bbe34654d0a1ced0e9d0
Raul T <jr.torsa@gmail.com>
2020-04-10 14:15:37 -0600

    Created home, about and base html filesn

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', {'title':'about'})
 
 def about(request):
     return render(request, 'blog/about.html')
